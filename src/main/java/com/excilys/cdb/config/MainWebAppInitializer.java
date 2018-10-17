b300eecda079a6c3130adbb9a1a53e3aebbce6f7
samoGit <srezzouqi@excilys.com>
2018-10-17 11:40:27 +0200

    [Spring] 1

---
+++ src/main/java/com/excilys/cdb/config/MainWebAppInitializer.java
@@ -0,0 +1,26 @@
+package com.excilys.cdb.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MainWebAppInitializer implements WebApplicationInitializer {
+	@Override
+	public void onStartup(final ServletContext sc) throws ServletException {
+
+		AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+
+		root.scan("com.excilys");
+		sc.addListener(new ContextLoaderListener(root));
+
+		ServletRegistration.Dynamic appServlet = sc.addServlet("mvc",
+				new DispatcherServlet(new GenericWebApplicationContext()));
+		appServlet.setLoadOnStartup(1);
+	}
+}
